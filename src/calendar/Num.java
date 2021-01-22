580fc169017d8c0eaf9e2bb0be30e4f2ba0036c0
kim-sung-jun <dd8873@naver.com>
2021-01-22 11:43:40 +0900

    calendar-num

---
+++ src/calendar/Num.java
@@ -0,0 +1,19 @@
+package calendar;
+
+import java.util.Scanner;
+
+//2021.01.22
+public class Num {
+    public static void main(String[] args) {
+
+        System.out.println("두 수를 입력하세요.");
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int num = a + b;
+
+        System.out.println("두 수의 합은 " + num + "입니다.");
+    }
+
+}
