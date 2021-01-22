7f95b16c8c415a5da95d9b3141797afe8ae90f63
kim-sung-jun <dd8873@naver.com>
2021-01-22 13:13:21 +0900

    calendar-num..

--- src/calendar/Num.java
+++ src/calendar/Num.java
@@ -14,7 +14,7 @@ public class Num {
         int num = a + b;
 
         System.out.println("두 수의 합은 " + num + "입니다.");
-//        System.out.println("%d와 %의 합은 %d입니다.",a,b, a + b); 대체 가능
+        //System.out.println("%d와 %의 합은 %d입니다.",a,b, a + b); 대체 가능
         scanner.close(); //해주는편이 좋다~
         //옆에 노란색(워닝?)은 가급적 없애주자 있으면 실행되더라도 좋지 않다~
     }
